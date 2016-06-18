eaf859421064564f63c5d36db7c803b369a04f8d
Kirill <opoxmeloid@yandex.ru>
2016-06-18 10:21:34 +0300

    Started rest api

--- server.js
+++ server.js
@@ -16,21 +16,7 @@ var conString = "postgres://app_rw:samplepass@localhost/testdb";
 //this initializes a connection pool
 //it will keep idle connections open for a (configurable) 30 seconds
 //and set a limit of 10 (also configurable)
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1
-  });
-});
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -46,13 +32,73 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 var messages = [];
 var sockets = [];
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("5min2home server listening at", addr.address + ":" + addr.port);
+});
+
+
+function custom_insert (src_lng, src_lat, dst_lng, dst_lat, user_id) {
+    // Copy paramters into query string und create new pg.client.query
+    console.log("fine\n");
+    var func_query = client.query("INSERT INTO test_from_to VALUES ( point(" + 
+        src_lng + ", " + src_lat + "), point(" + dst_lng + ", " + dst_lat + "), " + user_id + ");");
+    func_query.on('row', function(row) {
+        console.log(row);
+    });
+    func_query.on('end', client.end.bind(client));
+};
+
+function custom_select(f){
+    var client = new pg.Client(conString);
+    client.connect();
+    console.log("SELECT * was called");
+    var func_query = client.query("SELECT * FROM test_from_to;", function(err, result) {
+        console.log(result);
+        f(result.rows)
+        client.end();
+    });
+    var rows = [];
+    return rows;
+};
+
+function custom_select(f, lng, lat, rad) {
+    var client = new pg.Client(conString);
+    client.connect();
+    console.log("SELECT with params was called");
+    var func_query = client.query("SELECT * FROM test_from_to WHERE (src_lat - " + lat + ") * (src_lat - " + lat + 
+        ") - (src_lng - " + lng + ") * (src_lng - " + lng + " ) < " + rad * rad + " ;", 
+    function(err, result) {
+        console.log(result);
+        f(result.rows)
+        client.end();
+    });
+    var rows = [];
+    return rows;
+}
+
+//REST API 
+
+var restify = require('restify');
+
+function respond(req, res, next) {
+    res.send({'hello ': req.params.name});
+    next();
+};
+
+function respond_select(req,res, next) {
+    custom_select((t) => {
+        res.send(t);
+        next();
+    })
+};
+
+var server_api = restify.createServer();
+server_api.get('/hello/:name', respond);
+server_api.head('/hello/:name', respond);
+
+server_api.get('/select', respond_select);
+server_api.get('/select/:lat&:lng&:rad', respond_select);
+server_api.listen(8080, function() {
+  console.log('%s listening at %s', server_api.name, server_api.url);
 });
